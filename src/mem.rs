1d34aba8481efca326e87d90ca8b6c9b6dfeb7f8
Douman <douman@gmx.se>
2020-05-11 14:21:02 +0200

    0.2.0: Add special syntax to not create reference for values

--- src/mem.rs
+++ src/mem.rs
@@ -7,7 +7,7 @@ use core::mem;
 ///Returns size of the value at compile time.
 ///
 ///Doesn't work with unsized types as they can be only evaluated at runtime.
-pub const fn size_of<T>(_: &T) -> usize {
+pub const fn size_of<T: Sized>(_: &T) -> usize {
     mem::size_of::<T>()
 }
 
@@ -63,8 +63,23 @@ pub const fn needs_drop<T>(_: &T) -> bool {
 ///use type_traits::size_of;
 ///let array = [0u8; 4];
 ///assert_eq!(size_of!(array,), size_of!(u32));
+///
+/////When dealing with references, one should remember that Rust doesn't collapse references
+///fn test<T>(val: &T) -> usize {
+///    size_of!(val,)
+///}
+///assert_eq!(test(&255u8), size_of!(usize)); // this will cause to return ptr size
+///
+///fn test2<T>(val: &T) -> usize {
+///    //We use special syntax * to tell macro to not create extra reference
+///    size_of!(*val)
+///}
+///assert_eq!(test2(&255u8), size_of!(u8));
 ///```
 macro_rules! size_of {
+    (*$val:expr) => {
+        $crate::mem::size_of($val)
+    };
     ($type:ty) => {
         core::mem::size_of::<$type>()
     };
@@ -85,13 +100,18 @@ macro_rules! size_of {
 ///```
 ///use type_traits::align_of;
 ///let array = [0u8; 4];
+///let array_ref = &array;
 ///
 ///assert_eq!(align_of!(array[0]), align_of!(u8));
 ///assert_eq!(align_of!(array.len()), align_of!(usize));
 ///assert_eq!(align_of!(array,), align_of!(u8));
+///assert_eq!(align_of!(*array_ref), align_of!(u8));
 ///
 ///```
 macro_rules! align_of {
+    (*$val:expr) => {
+        $crate::mem::align_of($val)
+    };
     ($type:ty) => {
         core::mem::align_of::<$type>()
     };
@@ -113,12 +133,17 @@ macro_rules! align_of {
 ///use type_traits::needs_drop;
 ///let array = [0u8; 4];
 ///let string = "lolka".to_owned();
+///let string_ref = &string;
 ///
 ///assert!(!needs_drop!(array,));
 ///assert!(needs_drop!(string,));
+///assert!(needs_drop!(*string_ref));
 ///assert!(!needs_drop!(string.as_bytes()[0]));
 ///```
 macro_rules! needs_drop {
+    (*$val:expr) => {
+        $crate::mem::needs_drop($val)
+    };
     ($type:ty) => {
         core::mem::needs_drop::<$type>()
     };
